50b88866ac7f90b7153c428feb98d6c5712b64f6
farelamo <farizfahmi674@gmail.com>
2022-11-12 13:17:48 +0700

    update

--- components/inquiry.slice.js
+++ components/inquiry.slice.js
@@ -1,5 +1,9 @@
-import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
-import axios from 'axios'
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+import axios from "axios";
 
 // export const getInquiry = createAsyncThunk('/inquiry/getInquiry', async ({kode_produk, uid, pin, idpel1}) => {
 //     const payload = {
@@ -23,48 +27,79 @@ import axios from 'axios'
 // })
 
 export const inquiry = createSlice({
-    name: 'inquiry',
-    initialState: {
-        method: 'rajabiller.inq',
-        uid: '',
-        pin: '',
-        idpel1: '',
-        idpel2: '',
-        idpel3: '',
-        kode_produk: '',
-        ref1: '',
+  name: "inquiry",
+  initialState: {
+    method: "rajabiller.inq",
+    uid: "",
+    pin: "",
+    idpel1: "",
+    idpel2: "",
+    idpel3: "",
+    kode_produk: "",
+    ref1: "",
+  },
+  // initialState: inquiryEntity.getInitialState(),
+  reducers: {
+    updateInquiry: (state, action) => {
+      state.uid = action.payload.uid;
+      state.pin = action.payload.pin;
+      state.kode_produk = action.payload.kode_produk;
+      state.idpel1 = action.payload.idpel1;
+
+        const payload = {
+          method: "rajabiller.inq",
+          uid: "SP300203",
+          pin: "893456",
+          idpel1: "6130070696",
+          idpel2: "",
+          idpel3: "",
+          kode_produk: "GAS",
+          ref1: "",
+        };
+      //   const response = axios.post(
+      //     "https://rajabiller.fastpay.co.id/transaksi/json.php",
+      //     {
+      //       payload,
+      //     }
+      //   );
+
+      axios
+        .get("https://a.4cdn.org/a/threads.json", {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          proxy: {
+            host: "104.236.174.88",
+            port: 3128,
+          },
+          params: {
+            payload
+          }
+        })
+        .then(function (response) {
+          console.log("response is : " + response.data);
+        })
+        .catch(function (error) {
+          if (error.response) {
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log(error.message);
+          }
+          console.log(error.config);
+        });
+
+      //   console.log(response)
     },
-    // initialState: inquiryEntity.getInitialState(),
-    reducers: {
-        updateInquiry: (state, action) => {
-            state.uid = action.payload.uid
-            state.pin = action.payload.pin
-            state.kode_produk = action.payload.kode_produk
-            state.idpel1 = action.payload.idpel1
+  },
+  // extraReducers: {
+  //     [getInquiry.fulfilled]: (state, action) => {
+  //         inquiryEntity.setAll(state, action.payload)
+  //     }
+  // }
+});
 
-            const payload = {
-                "method": "rajabiller.inq",
-                "uid": "SP300203",
-                "pin": "893456",
-                "idpel1": "6130070696",
-                "idpel2": "",
-                "idpel3": "",
-                "kode_produk": "GAS",
-                "ref1": ""
-            }
-            const response = axios.post('https://api-partnerlink.bmsecure.id/json/index_devel.php', {
-                payload
-            })
-        }
-    }
-    // extraReducers: {
-    //     [getInquiry.fulfilled]: (state, action) => {
-    //         inquiryEntity.setAll(state, action.payload)
-    //     }
-    // }
-
-})
-
-export const { updateInquiry } = inquiry.actions
+export const { updateInquiry } = inquiry.actions;
 // export const inquirySelectors = inquiryEntity.getSelectors(state => state.inquiry)
-export default inquiry.reducer
+export default inquiry.reducer;
